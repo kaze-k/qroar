3aace44f3f5cb963383fe76ebaac2f1c66062b16
kaze <74790971+kaze-k@users.noreply.github.com>
2026-02-10 01:59:35 +0800

    chore: remove configuration comments

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,41 +1,28 @@
 // github: https://github.com/conventional-changelog/commitlint
-// options: https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+// options: https://commitlint.js.org/reference/configuration.html
 import type { UserConfig } from "@commitlint/types";
 
 export default {
-  // 继承的规则
   extends: ["@commitlint/config-conventional"],
-  // 规则
   rules: {
-    // 提交类型范围
     "type-enum": [
       2,
       "always",
       [
-        "feat", // 新功能
-        "fix", // 修复
-        "docs", // 文档变更
-        "style", // 代码格式
-        "refactor", // 代码重构
-        "perf", // 优化
-        "ci", // 持续集成
-        "test", // 测试用例
-        "chore", // 构建过程、依赖或辅助工具
-        "revert", // 代码回退
-        "merge", // 合并分支
-        "build", // 构建
-        "WIP", // 尚未完成
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "ci",
+        "test",
+        "chore",
+        "revert",
+        "merge",
+        "build",
+        "WIP",
       ],
     ],
-    // 主题大小写
-    "subject-case": [0],
-    // 类型大小写
-    "type-case": [0],
-    // 范围大小写
-    "scope-case": [0],
-    // header大小写
-    "header-case": [0],
-    // body大小写
-    "body-case": [0],
   },
 } satisfies UserConfig;
